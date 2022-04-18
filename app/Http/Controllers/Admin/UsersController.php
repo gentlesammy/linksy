b5fc12afcd705ff619ec85043b28cd74a73392cb
Gentlesammy <samodunlade84@gmail.com>
2022-04-18 06:05:37 +0100

    linkster admin home and css fixes

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -1,15 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Auth;
 class UsersController extends Controller
 {
     public function index(){
-        return view("admin.users.index");
+        $users = User::where("id", "!=", Auth::user()->id)->orderBy('created_at', 'desc')
+        ->orderBy('updated_at', 'asc')
+        ->paginate(20);
+        return view("admin.users.index", compact('users'));
+        
     }
 
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,79 @@
-<h1>Users Admin</h1>
+@extends('layouts.admin')
+
+@section("content")
+    <div class="hero">
+        <div class="container text-center bg-white h-50 py-5">
+        <h1 class="">Manage Users </h1>
+        <div class="options">
+                <a href="/admin">Go Back to Admin Dashboard</a>
+        </div>
+        </div>
+    </div>
+
+    <div class="container">
+    <div class="row">
+        @if (count($users) > 0)
+            <div class="table-responsive">
+                <table class="table table-bordered mb-5">
+                    <thead>
+                        <tr class="table-success">
+                            <th scope="col">#</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Role</th>
+                            <th scope="col">Total Visit</th>
+                            <th scope="col">Created</th>
+                            <th scope="col">Manage User</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php $sn = 1; ?>
+                            @foreach($users as $data)
+                                <tr>
+                                    <th scope="row">{{$sn++}}</th>
+                                    <td>{{ $data->username }}</td>
+                                    <td>{{ $data->email }}</td>
+                                    <td>{{ $data->description }}</td>
+                                    <td>{{ $data->role }}</td>
+                                    <td>{{ $data->total_visit_count }}</td>
+                                    <td>{{ $data->created_at}}</td>
+                                    <td>
+                                    <a href="/dashboard/links/{{$data->id}}" class="btn btn-primary"><i class="fa fa-pencil-square-o text-white" aria-hidden="true"></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @else
+            <div class="noLink text-center" style="text-align:center !important;">
+                <h3 class=" py-3 px-2 text-info">No Registered User in the Database</h3>
+            </div>
+        @endif
+        {{-- Pagination --}}
+        <div class="d-flex justify-content-center">
+            {!! $users->links() !!}
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
